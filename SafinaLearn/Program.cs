94de658aa4de14e46a1196f0ef096b03c7c4a3c2
guzeev.mikhail <guzeev.md@gmail.com>
2023-07-05 03:09:22 +0400

    feat: first steps

--- SafinaLearn/Program.cs
+++ SafinaLearn/Program.cs
@@ -10,6 +10,58 @@ namespace SafinaLearn
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите x");
+            string inputX = Console.ReadLine();
+            Console.WriteLine("Введите y");
+            string inputY = Console.ReadLine();
+            int x = Convert.ToInt32(inputX);
+            int y = Convert.ToInt32(inputY);
+
+            //if ( x == 30 && x == 50 )
+            //{
+            //    Console.WriteLine("30 и 50");
+            //}
+
+            //if ( x > 30 || x < 15)
+            //{
+            //    Console.WriteLine("x больше 30 или меньще 15");
+            //}
+            //if ((x <= 10 || x > 20) && x != 15 )
+            //{
+            //    Console.WriteLine("(x меньше или равно 10 или больше 20) и x не равно 15");
+            //}
+            //if ((x < y) || (x < 20 && x > 10 && y > 10) && x != 0 && y != 0)
+            //{
+            //    Console.WriteLine("(X меньше Y) или (X меньше 20 и больше 10 и Y больше 10) и X не равно 0 и Y не равно 0");
+            //}
+
+            //switch (x)
+            //{
+            //    case 30:
+            //        Console.WriteLine("мама по попе даст");
+            //        break;
+            //    case 50:
+            //        Console.WriteLine("куда тебе такой старик");
+            //        break;
+            //    default:
+            //        Console.WriteLine("не попал");
+            //        break;
+            //}
+
+            //if (x > 50)
+            //{
+            //    Console.WriteLine("МНЕ БОЛЬШЕ 50 АЛО");
+            //}
+            //else if (x > 30)
+            //{
+            //    Console.WriteLine("то вам попа от мамы");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("нормик");
+            //}
+            Console.ReadLine();
         }
     }
 }
+  
